c89b71863caec36fe83849b4ff04d4ac6596a284
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-12 21:48:08 +0000

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function to start program
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
