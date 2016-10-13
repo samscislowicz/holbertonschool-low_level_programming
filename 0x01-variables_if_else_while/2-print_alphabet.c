98d76f2ee666e080e9a114252ad5fb14079fb90d
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-13 20:03:53 +0000

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function to start program
+ * Return: return messeage
+ */
+int main(void)
+{
+	char z = 'a';
+
+	while (z <= 'z')
+	{
+		putchar(z);
+		z++;
+	}
+	putchar('\n');
+	return (0);
+}
