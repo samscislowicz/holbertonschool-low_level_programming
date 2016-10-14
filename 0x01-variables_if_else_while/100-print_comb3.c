a4640f47c1239db066f6bb7f59c562497977232e
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-14 19:49:04 +0000

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - print 2 digit combos
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int f = '0';
+	int s = '0';
+
+	while (f <= '8')
+	{
+		s = '0';
+		while (s <= '9')
+		{
+			putchar(f);
+			putchar(s);
+			if (s == '9' && f == '8')
+			{
+			}
+			if (f !> s)
+			{
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			s++;
+		}
+		f++;
+	}
+	putchar('\n');
+	return (0);
+}
