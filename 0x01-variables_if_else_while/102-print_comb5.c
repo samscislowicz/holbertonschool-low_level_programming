2ee1b4e75d29c7a80346af9228aeb3a6c6a9338d
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-14 23:39:48 +0000

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+/**
+ * main - print 2 digit combos
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '0';
+	int c = '0';
+	int d;
+	int temp = '0';
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		for (b = '0' ; b <= '9' ; b++)
+		{
+			for (c = '0' ; c <= '9' ; c++)
+			{
+				if (a == c)
+				{
+					temp = b + 1;
+				}
+				else
+				{
+					temp = '0';
+				}
+				for (d = temp ; d <= '9' ; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if (!(a == '9' && b == '8' && c == '9' && d == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+		putchar('\n');
+		return (0);
+}
