121ce08816debed91f01dbe1e9a4f9d2472bae14
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-18 16:50:45 +0000

    commit

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,30 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char c);
+/* Task 1 */
+void print_alphabet(void);
+/* Task 2 */
+void print_alphabet_x10(void);
+/* Task 3 */
+int _islower(int c);
+/* Task 4 */
+int _isalpha(int c);
+/* Task 5 */
+int print_sign(int n);
+/* Task 6 */
+int _abs(int n);
+/* Task 7 */
+int print_last_digit(int n);
+/* Task 8 */
+void jack_bauer(void);
+/* Task 9 */
+void times_table(void);
+/* Task 10 */
+int add(int i, int j);
+/* Task 11 */
+void print_to_98(int n);
+/* Task 12 */
+void print_times_table(int n);
+
+#endif
