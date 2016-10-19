ec8b1f5bb009a655668eba46a268a11b13ca0864
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-19 00:56:55 +0000

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - add 2 numbers
+ * @i: first operand
+ * @j: second operand
+ * Return: sum of i and j
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+	return (k);
+}
