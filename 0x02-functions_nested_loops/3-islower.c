c195c206c4bdcb4110e201e00dc98b283eb7d435
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-19 00:20:34 +0000

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,5 +1,10 @@
-#include "holberton"
+#include "holberton.h"
 
+/**
+ * _islower - returns 1 for lowercase, 0 for other
+ * @c: first operand
+ * Return: 1 or 0
+ */
 int _islower(int c)
 {
 	if ( c >= 'a' && c <= 'z')
