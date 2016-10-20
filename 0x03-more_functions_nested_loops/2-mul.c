ca4a738c1e3ea10bc29f2bddfbad8490f26e6896
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-20 21:57:35 +0000

    commit

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 
 /**
- * mul - multiplies 2 variables
- * @a: variable to be multipled by b
- * Return: 0
- */
+* mul - multiplies 2 variables
+* @b: variable to be multipled by a
+* @a: variable to be multipled by b
+* Return: 0
+*/
+
 int mul(int a, int b)
 {
 	return(a * b);
