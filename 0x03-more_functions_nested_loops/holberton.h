c078eda03c1cd2628d3afea42b393a143fa59b92
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-20 16:19:38 +0000

    commit

---
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _isupper(int c);
+/**Task 0*/
+int _isdigit(int c);
+/**Task 1*/
+int mul(int a, int b);
+/**Task 2*/
+void print_numbers(void);
+/**Tasks 3*/
+void print_most_numbers(void);
+/**Task 4*/
+void more_numbers(void);
+/**Task 5*/
+void print_line(int n);
+/**Task 6*/
+void print_diagonal(int n);
+/**Task 7*/
+void print_square(int size);
+/**Task 8*/
+void print_triangle(int size);
+/**Task 10*/
