bef2bf5865ba8eef0990ac41ad08c6e7e2460b9d
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-20 22:50:11 +0000

    commit

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,12 @@ int main(void)
 	for (i = 1; i <= 100; ++i)
 	{
 		if (i % 3 == 0)
-			printf("Fizz");
-		if (i % 5 == 0)
-			printf("Buzz");
-		if ((i % 3 != 0) && (i % 5 != 0))
-			printf("%d", i);
-		printf(" ");
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else  ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d ", i);
 	}
+	printf("\n");
 	return (0);
 }
