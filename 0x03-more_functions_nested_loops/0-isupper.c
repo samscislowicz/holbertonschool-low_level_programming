4fd9b880fe6d2504daac7da627203b5aebcfa9de
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-20 17:51:33 +0000

    commit

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isupper - return 1 if uppercase, 0 if otherwise
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
