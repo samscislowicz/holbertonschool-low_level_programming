0091fed9b735cc8e7f2dc9cb745d368a6ce09f80
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-25 17:30:16 +0000

    headerfile

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,20 @@
+void reset_to_98(int *n);
+/**Task 0*/
+void swap_int(int *a, int *b);
+/**Task 1*/
+int _strlen(char *s);
+/**Tasks 2*/
+void _puts(char *str);
+/**Task 3*/
+void print_rev(char *s);
+/**Task 4*/
+void rev_string(char *s);
+/**Tasks 5*/
+void puts2(char *str);
+/**Task 6*/
+void puts_half(char *str);
+/**Task 7*/
+void print_array(int *a, int n);
+/**Task 8*/
+char *_strcpy(char *dest, char *src);
+/**Task 9*/
