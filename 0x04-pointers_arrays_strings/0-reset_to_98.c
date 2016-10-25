8710dde089bbb83a715c3905f914d3b959611246
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-25 17:57:22 +0000

    removed include

--- 0x04-pointers_arrays_strings/0-reset_to_98.c
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,6 @@
-#include "holberton.h"
-
 /**
  * reset_to_98 - Set value of a variable to 98 using a pointer
  * @n: The address of the value needed to change
- *
  * Return: nothing
  */
 void reset_to_98(int *n)
