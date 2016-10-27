6db5359bcb00aa488c2e3a1210ff7711bb7b70f7
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-27 15:54:43 +0000

    upload

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+char *_strcat(char *dest, char *src);
+/**Task 0*/
+char *_strncat(char *dest, char *src, int n);
+/**Task 1*/
+char *_strncpy(char *dest, char *src, int n);
+/**Task 2*/
+int _strcmp(char *s1, char *s2);
+/**Task 3*/
+void reverse_array(int *a, int n);
+/**Task 4*/
+char *string_toupper(char *);
+/**Task 5*/
+char *cap_string(char *);
+/**Task 6*/
+char *leet(char *);
+/**Task 7*/
+char *rot13(char *);
+/**Task 8*/
