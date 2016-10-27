81ecec721dc80e8121ae78718492242b3e49078a
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-10-27 17:08:30 +0000

    upload

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strcmp: compares two strings
+ * @s1: string one to compare
+ * @s2: string two to compare
+ * Return: an integer less than 0 if s1 is found
+ */
+
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (s1[i] >  s2[i])
+			return (15);
+		if (s1[i] < s2[i])
+			return (-15);
+		i++;
+	}
+	return (0);
+}
