092ac6eccddf3414748882ab7f76048447e87ad3
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-11-17 17:46:14 +0000

    initial commit

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * op_add - adds;
+ * @a: int 1
+ * @b: int 2
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract;
+ * @a : int 1
+ * @b: int 2
+ * Return: subtraction asnwer
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies;
+ * @a: int 1
+ * @b: int 2
+ * Return: multiplication answer
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - op div;
+ * @a: int 1
+ * @b: int 2
+ * Return: division answer
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod func
+ * @a: int 1
+ * @b: int 2
+ * Return: mod
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
