7e3d5c82b0399ff2f05af2f127f87aec99a2ec7e
Sam Scislowicz <samantha.scislowicz@gmail.com>
2016-12-01 23:56:23 +0000

    first commit

---
+++ 0x12-more_slingly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *free_listint - free a list
+ *@head: pointer to the head of the list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
