d3d2df9d01a5ac4397ff1c158bcdca68af687610
Sam Scislowicz <samantha.scislowicz@gmail.com>
2017-02-23 22:48:35 +0000

    update documentation

--- 0x1A-sorting_algorithms/0-bubble_sort.c
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
- *
+ * bubble_sort - Sort by swapping largest items to end of array
+ * @array: Array to be sorted
+ * @size: Size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
@@ -13,6 +13,8 @@ void bubble_sort(int *array, size_t size)
 	size_t temp;
 	size_t tempsize;
 
+	if (array == NULL)
+		return;
 	tempsize = size;
 	swapped = 1;
 	while(swapped)
